cd198b0ad58a919ffc66d470e94bf207115222a9
ramzi-omari <ramziomari1995@gmail.com>
2021-02-01 23:57:01 +0100

    Template Engine 'handlebars' and layouts

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
+const exphbs = require("express-handlebars");
 const connectDB = require("./config/db");
 
 // Load config
@@ -11,9 +11,14 @@ connectDB();
 
 const app = express();
 
+// Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// Handlebars
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
 const PORT = process.env.PORT || 3000;
 
