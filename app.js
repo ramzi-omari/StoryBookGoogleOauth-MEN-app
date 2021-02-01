bbe106c3ec6ad34cab9896f9d93eb8c1949cfaf8
ramzi-omari <ramziomari1995@gmail.com>
2021-02-01 23:40:54 +0100

    Morgan logger setup to log http request..

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+
 const connectDB = require("./config/db");
 
 // Load config
@@ -8,6 +10,10 @@ dotenv.config({ path: "./config/config.env" });
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 const PORT = process.env.PORT || 3000;
 
