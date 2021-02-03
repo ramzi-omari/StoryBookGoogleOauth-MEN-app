90b1d6dad47b9f0f5885799d34821e5ef6d8434d
ramzi-omari <ramziomari1995@gmail.com>
2021-02-03 11:03:41 +0100

    Store sessions in Database 'to maintain loggin when refresh'

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
 const passport = require("passport");
 const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const connectDB = require("./config/db");
 
 // Load config
@@ -27,11 +29,13 @@ app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
 // Sessions (should be above passport middleware)
+// store to save our sessions (cookies) in the database to avoid loggin out when we refresh the page
 app.use(
   session({
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 );
 
